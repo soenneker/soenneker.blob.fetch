62ee974f38e363ba9d8b1b3a226e413c90305080
Jake Soenneker <jake@soenneker.com>
2024-07-04 19:28:43 -0500

    CancellationToken support

--- src/BlobFetchUtil.cs
+++ src/BlobFetchUtil.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Logging;
 using Soenneker.Blob.Container.Abstract;
 using Soenneker.Blob.Fetch.Abstract;
 using Soenneker.Extensions.ValueTask;
-using Soenneker.Utils.Cancellation.Abstract;
 
 namespace Soenneker.Blob.Fetch;
 
@@ -15,24 +14,19 @@ namespace Soenneker.Blob.Fetch;
 public class BlobFetchUtil : IBlobFetchUtil
 {
     private readonly ILogger<BlobFetchUtil> _logger;
-    private readonly ICancellationUtil _cancellationUtil;
     private readonly IBlobContainerUtil _blobContainerUtil;
 
-    public BlobFetchUtil(ILogger<BlobFetchUtil> logger, IBlobContainerUtil blobContainerUtil, ICancellationUtil cancellationUtil)
+    public BlobFetchUtil(ILogger<BlobFetchUtil> logger, IBlobContainerUtil blobContainerUtil)
     {
         _logger = logger;
         _blobContainerUtil = blobContainerUtil;
-        _cancellationUtil = cancellationUtil;
     }
-        
+
     public async ValueTask<List<BlobItem>> GetAllBlobItems(string blobContainer, string? prefix = null, CancellationToken cancellationToken = default)
     {
-        _logger.LogDebug("Getting all blob items from container {container} with prefix {prefix}", blobContainer, prefix);
+        _logger.LogDebug("Getting all blob items from container ({container}) with prefix ({prefix})...", blobContainer, prefix);
 
-        BlobContainerClient container = await _blobContainerUtil.Get(blobContainer).NoSync();
-
-        if (cancellationToken == CancellationToken.None)
-            cancellationToken = _cancellationUtil.Get();
+        BlobContainerClient container = await _blobContainerUtil.Get(blobContainer, cancellationToken: cancellationToken).NoSync();
 
         var blobItems = new List<BlobItem>();
 
@@ -41,7 +35,7 @@ public class BlobFetchUtil : IBlobFetchUtil
             blobItems.Add(blobItem);
         }
 
-        _logger.LogDebug("Completed getting all blob items in container {container} with prefix: {prefix}", blobContainer, prefix);
+        _logger.LogDebug("Completed getting all blob items in container ({container}) with prefix ({prefix})", blobContainer, prefix);
 
         return blobItems;
     }
--- src/Registrars/BlobFetchUtilRegistrar.cs
+++ src/Registrars/BlobFetchUtilRegistrar.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.Blob.Container.Registrars;
 using Soenneker.Blob.Fetch.Abstract;
-using Soenneker.Utils.Cancellation.Registrars;
 
 namespace Soenneker.Blob.Fetch.Registrars;
 
@@ -14,7 +13,6 @@ public static class BlobFetchUtilRegistrar
     public static void AddBlobFetchUtilAsScoped(this IServiceCollection services)
     {
         services.AddBlobContainerUtilAsSingleton();
-        services.AddCancellationUtil();
         services.TryAddScoped<IBlobFetchUtil, BlobFetchUtil>();
     }
 }
